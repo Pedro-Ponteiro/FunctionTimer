8618adadb4b990d6a83500e374df08ab9403a7ae
pedro-ponteiro <silv9797@gmail.com>
2022-02-02 15:38:49 -0300

    Initial commit

---
+++ comparator.py
@@ -0,0 +1,103 @@
+import timeit
+from typing import Any, Dict, List, Protocol, Union
+
+
+class GenericFunction(Protocol):
+    def __call__(self, **kwds: Any) -> Any:
+        ...
+
+
+class FunctionsTimer:
+    def __setup_attributes(self) -> None:
+        # Dictionary with keys as function names and values as a list of times
+        self.funcs_times: Dict[str, List[float]] = {}
+
+        # Dictionary with keys as executed codes and values as a list of times
+        self.code_times: Dict[str, List[float]] = {}
+
+        # Dictionary that has both information. See time_funcs doc for details
+        self.funcs_code_times: Dict[str, Dict[str, Union[str, List[float]]]] = {}
+
+    def time_funcs(
+        self,
+        functions: Dict[GenericFunction, Dict[str, Any]],
+        repeat_times: int = 5,
+        num_trials: int = 1_000_000,
+    ) -> Dict[str, Dict[str, List]]:
+        """Times functions execution
+
+        Args:
+            functions (Dict[GenericFunction, Dict[str, Any]]):
+                    {
+                        function_to_be_analyzed: {  # empty if function does not accept any parameters
+                            'parameter_name': param_value,
+                            'parameter_name': param_value,
+                            .
+                            .
+                            .
+                        }
+                        function_to_be_analyzed: {  # empty if function does not accept any parameters
+                            'parameter_name': param_value,
+                            'parameter_name': param_value,
+                            .
+                            .
+                            .
+                        }
+                        .
+                        .
+                        .
+                    }
+            repeat_times (int, optional): Number of repeats. Defaults to 5.
+            num_trials (int, optional): Number of times the function is called in
+            each repeat. Defaults to 1_000_000.
+
+        Returns:
+            Dict[str, Dict[str, List]]:
+
+                {
+                    function_name: {
+                        'code_execution': code that got executed: str,
+                        'times': List of timed execution results
+                    }
+                    function_name {
+                        'code_execution': code that got executed: str,
+                        'times': List of timed execution results
+                    }
+                    .
+                    .
+                    .
+                }
+        """
+
+        self.__setup_attributes()
+
+        for func, args in functions.items():
+
+            arg_correspondents = [f"{arg_name}={arg}" for arg_name, arg in args.items()]
+
+            setup = ";".join(arg_correspondents)
+
+            parameters = ",".join(arg_correspondents)
+
+            globals()[func.__name__] = func
+
+            func_str = f"{func.__name__}({parameters})"
+
+            times = timeit.repeat(
+                stmt=func_str,
+                setup=setup,
+                globals=globals(),
+                repeat=repeat_times,
+                number=num_trials,
+            )
+
+            self.funcs_code_times[func.__name__] = {
+                "code_executed": func_str,
+                "times": times,
+            }
+
+            self.funcs_times[func.__name__] = times
+
+            self.code_times[func_str] = times
+
+        return self.funcs_code_times
---
+++ examples.py
@@ -0,0 +1,78 @@
+from pprint import pprint
+from random import shuffle
+from typing import List
+
+from comparator import Functions_Timer
+
+
+def sorted_sol():
+    a = [7, 3, 2, 5, 1, 4, 9, 8, 6]
+    shuffle(a)
+
+    a = sorted(a)
+    return a == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def set_sol():
+
+    a = [7, 3, 2, 5, 1, 4, 9, 8, 6]
+    shuffle(a)
+
+    a = set(a)
+    return a == {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+
+def sorted_with_params(a: List[int]):
+    shuffle(a)
+
+    a = sorted(a)
+    return a == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def set_with_params(a: List[int]):
+    shuffle(a)
+
+    a = set(a)
+    return a == {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+
+def main():
+
+    ft = Functions_Timer()
+
+    # example with function without params
+    times = ft.time_funcs(
+        {
+            sorted_sol: {},
+            set_sol: {},
+        },
+        repeat_times=2,  # optional
+        num_trials=1_000_000,  # optional
+    )
+    print("Example function without params")
+    pprint(times)
+    print()
+    # example with function with params
+    times = ft.time_funcs(
+        {
+            sorted_with_params: {"a": [1, 2, 3, 4, 5]},
+            set_with_params: {"a": [1, 2, 3, 4, 5]},
+        },
+        repeat_times=2,  # optional
+        num_trials=1_000_000,  # optional
+    )
+    print("Example function with params")
+    pprint(times)
+
+    print("\nAccessing different object attributes:")
+
+    print("\ncode_times")
+    print(ft.code_times)
+    print("\nfuncs_code_times")
+    print(ft.funcs_code_times)
+    print("\nfuncs_times")
+    print(ft.funcs_times)
+
+
+if __name__ == "__main__":
+    main()
